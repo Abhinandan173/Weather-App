aed2c463bcda59a80cf9bdc67669f49af2736e5c
Abhinandan <abhibaloji7@gmail.com>
2021-05-19 19:50:41 +0530

    my commit

--- src/components/tempapp.js
+++ src/components/tempapp.js
@@ -13,38 +13,30 @@ const Tempapp = () => {
         }
         fetchApi()
     }, [search])
-
-    // useEffect(() =>{
-    //     const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=3f343b19f6110ff72c103d9a1c344276`
-    //     axios.get(url).then(response =>{
-    //         const aa = response.json()
-    //         setcity(aa.main)
-    //     })
-    // }, [search])
     return (
         <>
             <div className="container mt-5 ">
                 <div className="row">
-<div className="col-lg-3"></div>
-<div className="col-lg-6 py-3"  id="container">
-    <h2 className="text-danger text-center " id="heading">Weather APP</h2>
-<input type="search" className="form-control mt-3 text-center"
-placeholder="Enter City Name"
-        value={search}
-        onChange={(obj) => { setsearch(obj.target.value) }}
-    />
-    {
-!city ? (
-    <p>No Data Found</p>
-) : (
-    <div className="row">
-        <div className="col-lg-6 offset-lg-3 text-center">
-<h1> {city.temp}°C</h1>
-<h2 className="text-primary mt-2"><i className="fas fa-cloud-sun"></i>{search}</h2>
-<p>Max : {city.temp_max}°C <br /> Min : {city.temp_min}°C </p>
-                            </div>
-                        </div>
-                    )}
+                    <div className="col-lg-3"></div>
+                    <div className="col-lg-6 py-3" id="container">
+                        <h2 className="text-danger text-center " id="heading">Weather APP</h2>
+                        <input type="search" className="form-control mt-3 text-center"
+                            placeholder="Enter City Name"
+                            value={search}
+                            onChange={(obj) => { setsearch(obj.target.value) }}
+                        />
+                        {
+                            !city ? (
+                                <p>No Data Found</p>
+                            ) : (
+                                <div className="row">
+                                    <div className="col  text-center">
+                                        <h1> {city.temp}°C</h1>
+                                        <h2 className="text-primary mt-2"><i className="fas fa-cloud-sun"></i>{search}</h2>
+                                        <p>Max : {city.temp_max}°C <br /> Min : {city.temp_min}°C </p>
+                                    </div>
+                                </div>
+                            )}
                     </div>
                 </div>
             </div>
